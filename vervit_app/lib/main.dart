bb3b64c2bd03c93ea026008cf1a17ce7af3762d2
latka-krystof <74324358+latka-krystof@users.noreply.github.com>
2021-04-12 21:27:34 +0200

    modify main.dart

--- vervit_app/lib/main.dart
+++ vervit_app/lib/main.dart
@@ -33,7 +33,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[800],
         title: Text('Hello World!'),
